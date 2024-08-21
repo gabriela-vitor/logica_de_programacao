dcdb2fb08483ef3fec03e7337cf02132aa0ef127
Gabriela <127972306+gabriela-vitor@users.noreply.github.com>
2024-08-21 09:50:24 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+	
+	int idade;
+	int peso;
+	int ano;
+	int dia;
+	int mes;
+	int ddd;
+	
+	
+	printf("digite sua idade\n");// printar o digite sua idade
+	scanf("%d", &idade); //digitar os dados de entrada
+	printf("idade digitada: %d\n\n", idade); //printar o idade digitada (junto com os dados de entrada)
+	
+	printf("digite seu peso\n");
+	scanf("%d", &peso);
+	printf("peso digitado: %d\n\n", peso);
+	
+	printf("digite seu ano\n");
+	scanf("%d", &ano);
+	printf("ano digitado: %d\n\n", ano);
+	
+	printf("digite seu dia\n");
+	scanf("%d", &dia);
+	printf("dia digitado: %d\n\n", dia);
+	
+	printf("digite seu mes\n");
+	scanf("%d", &mes);
+	printf("mes digitado: %d\n\n", mes);
+	
+	printf("digite seu ddd\n");
+	scanf("%d", &ddd);
+	printf("ddd digitado: %d\n\n", ddd);
+	
+	
+	
+	
+	
+	
+}
